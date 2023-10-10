5bda64ea422210519de23e3116e102aee8ae0b40
dondou21 <dondouabiyi@gmail.com>
2023-10-10 04:58:50 -0700

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * *print_alphabet - function that print alphabet
- * *it prints in lowercase
- * *Return: no return
- * */
+ *print_alphabet - function that print alphabet
+ *it prints in lowercase
+ *Return: no return
+ */
 void print_alphabet(void)
 {
 	int letter;
